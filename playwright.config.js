74a857e89945990274f2e1ccf950a1e828d08cb1
vikrantpathlock <vikrant.bulbule@pathlock.com>
2026-07-28 16:28:36 +0530

    Cucumber code

--- playwright.config.js
+++ playwright.config.js
@@ -1,81 +1,37 @@
-// @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { chromium, defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
+// All Configuration key value pair is stored in this
+// we need to export this defineConfig so that it will available to all files in the project
 export default defineConfig({
+  //const  config=({
   testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  //by default timeout is of 30 sec 
+  // only if we want to overwrite the default timeout we need to defined it otherwise no need
+  // Timeout of 40 sec to wait for perticular element
+  // this time out is for every steps in playwright and components
+  timeout:40 *1000,
+  // for assertion validation we use expect timeout
+  expect:{
+  timeout: 5000,
+  },
+ // Once all the test run below line give the report in html
+ reporter:'html',
+  
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+   
+    // Give the browser name where we want to run the test
+    // In beloe way we can do the cross browser testing
+    // to run in chrome
+    browserName:'chromium',
+    
+    headless: false,
+    //To get the screenshots for every step in playwright Execution
+    screenshot :'on',
+    trace : 'retain-on-failure'//off,on,retain-on-failure
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    
   },
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
-
+//module.exports - config
